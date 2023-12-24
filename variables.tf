dcad98e348ee15c157b00373fffbe71d557602f3
EC2 Default User <ec2-user@ip-172-31-74-108.ec2.internal>
2023-12-24 15:01:35 +0000

    abd-key

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "key_name" {
+  type = string
+  }
+
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "num_of_instance" {
+  type = number
+  default = 1
+}
+
+variable "tag" {
+  type = string
+  default = "Docker-Instance"
+}
+
+
+variable "server-name" {
+  type = string
+  default = "docker-instance"
+}
+
+variable "docker-instance-ports" {
+  type = list(number)
+  description = "docker-instance-sec-gr-inbound-rules"
+  default = [22, 80, 8080]
+}
